f2b200035c4cc1293dcb697ee0dd54586fe73beb
Jekaterina Shakhnazarova <jekaterina.peredelska@gmail.com>
2018-07-27 23:47:15 +0300

    Исправляю ошибки.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
-var openClose = document.querySelector(".write-us-btn"),
-    popup = document.querySelector(".modal-content"),
-    close = document.querySelector(".modal-content-close"),
-    form = popup.querySelector("form"),
-    username = popup.querySelector("[name='username']"),
-    email = popup.querySelector("[name='e-mail']"),
-    letter = popup.querySelector(".letter"),
-    storage = localStorage.getItem("username");
-openClose.addEventListener("click", function(e) {
-    popup.classList.contains("modal-content-show") ? (e.preventDefault(), popup.classList.remove("modal-content-show")) : (e.preventDefault(), popup.classList.remove("modal-error"), popup.classList.add("modal-content-show"), storage ? (username.value = storage, email.focus()) : username.focus())
-}), close.addEventListener("click", function(e) {
-    e.preventDefault(), popup.classList.remove("modal-content-show")
-}), window.addEventListener("keydown", function(e) {
-    27 == e.keyCode && popup.classList.contains("modal-content-show") && popup.classList.remove("modal-content-show")
-}), form.addEventListener("submit", function(e) {
-    username.value && email.value && letter.value ? (localStorage.setItem("username", username.value), popup.classList.remove("modal-content-show")) : (e.preventDefault(), popup.classList.add("modal-error"))
+var link = document.querySelector(".button-write-us");
+
+var popup = document.querySelector(".modal-contact");
+var close = document.querySelector(".modal-close");
+
+var login = popup.querySelector("[name=login]");
+
+
+link.addEventListener("click", function (evt) {   
+	evt.preventDefault();				
+	console.log("Click on write us buton");
+	popup.classList.remove("visually-hidden"); 			
+	login.focus(); 							
 });
+
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("visually-hidden");
+});
+
+
+function validateForm() {
+    var x = document.forms["contact-form"]["name"].value;
+    if (x == "") {
+		document.forms["contact-form"]["name"].classList.add("error");
+        return false;
+    }
+} 
